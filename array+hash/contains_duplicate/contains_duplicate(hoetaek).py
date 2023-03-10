a3a81e7670cd7e0a72f751723cf59a1de6e7ef73
hoetaek <hoetaek@student.snue.ac.kr>
2023-03-10 19:57:36 +0900

    contains_duplicate

--- array+hash/contains_duplicate/contains_duplicate(hoetaek).py
+++ array+hash/contains_duplicate/contains_duplicate(hoetaek).py
@@ -1,1 +1,11 @@
-print("test")
+from typing import List
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        hs = set()
+        for i in nums:
+            if i not in hs:
+                hs.add(i)
+            else:
+                return True
+        return False
