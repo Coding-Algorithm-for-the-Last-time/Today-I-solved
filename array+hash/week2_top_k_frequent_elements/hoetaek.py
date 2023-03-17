fd1874de24a5434aa09fd3c5cb78bfd1174e8685
hoetaek <hoetaek@student.snue.ac.kr>
2023-03-17 22:37:21 +0900

    second solve

--- array+hash/week2_top_k_frequent_elements/hoetaek.py
+++ array+hash/week2_top_k_frequent_elements/hoetaek.py
@@ -8,3 +8,10 @@ class Solution:
         for num in nums:
             hs[num] += 1
         return [i[0] for i in sorted(hs.items(), key=lambda x: x[1], reverse=True)[:k]]
+
+from collections import Counter
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        n = Counter(nums)
+        return [i[0] for i in n.most_common(k)]
