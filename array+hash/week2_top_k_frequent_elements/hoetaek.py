cf56401fb9ed188b32bd9d978df62581c3620ca7
hoetaek <hoetaek@student.snue.ac.kr>
2023-03-17 22:53:51 +0900

    third solve

--- array+hash/week2_top_k_frequent_elements/hoetaek.py
+++ array+hash/week2_top_k_frequent_elements/hoetaek.py
@@ -15,3 +15,21 @@ class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         n = Counter(nums)
         return [i[0] for i in n.most_common(k)]
+    
+from collections import defaultdict
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = {}
+        for num in nums:
+            count[num] = count.get(num, 0) + 1
+        freq = [[] for _ in range(len(nums) + 1)]
+        for n, c in count.items():
+            freq[c].append(n)
+
+        answer = []
+        for i in range(len(freq) - 1, 0, -1):
+            for j in freq[i]:
+                answer.append(j)
+                if len(answer) == k:
+                    return answer
