e3e9f5eeeb584951c4f3ea75c49d30dd3678f226
teacher-kiwi <kwi07@hanmail.net>
2023-03-18 16:54:54 +0900

    2주차 코드 이동(hd, teacher-kiwi) - 기존 폴더 삭제

--- array+hash/top_k_frequent_elements/teacher-kiwi.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        num_dict = {}
-        for num in nums:
-            if num in num_dict: num_dict[num][1] += 1
-            else: num_dict[num] = [num, 1]
-        return [li[0] for li in sorted(num_dict.values(), key=lambda x: x[1], reverse=True)[:k]]
