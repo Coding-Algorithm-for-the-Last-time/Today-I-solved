c8004a9a0267d74a9e06172ee82d4fab3924957f
jun <lhj881023@gmail.com>
2023-03-25 13:43:31 +0900

    3주차미완

--- array+hash/week3_longest_substring_without_repeating_characters/juniya.py
+++ array+hash/week3_longest_substring_without_repeating_characters/juniya.py
@@ -1,18 +1,16 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        char_set = set()
+        left = 0
+        right = 0
+        max_length = 0
 
-s= 'pwwkew'
-
-print(list(enumerate(s)))
-
-# index = {"start_index" : 0, "finish_index" : 0}
-
-# length = []
-
-# sub_s= ""
-# for i in s:
-#     if i in sub_s:
-#         index["start_index"] += 1
-#     sub_s = sub_s + i
-#     index["finish_index"] +=1
-#     length.append(index["finish_index"] - index["start_index"])
-
-# print(max(length))
+        while right < len(s):
+            if s[right] not in char_set:
+                char_set.add(s[right])
+                right += 1
+                max_length = max(max_length, right - left)
+            else:
+                char_set.remove(s[left])
+                left += 1
+        return max_length
