0ba1fa54ce22cf82b97435afa83a5af1cb8bc3e7
nlom0218 <nlom0218@naver.com>
2023-03-25 17:41:52 +0900

    docs: update

--- array+hash/week3_first_missing_positive/hd.js
+++ array+hash/week3_first_missing_positive/hd.js
@@ -6,13 +6,11 @@ var firstMissingPositive = function (nums) {
   nums = nums.filter((num) => num > 0).sort((a, b) => a - b);
   nums = [...new Set(nums)];
 
-  let answer = 0;
   for (let i = 0; i < nums.length; i++) {
     if (nums[i] !== i + 1) {
-      answer = i + 1;
-      break;
+      return i + 1;
     }
   }
 
-  return !answer ? nums.length + 1 : answer;
+  return nums.length + 1;
 };
