08d39044d72779d5ed42cf42027345164742b038
teacher-kiwi <kwi07@hanmail.net>
2023-04-06 00:40:10 +0900

    코드 추가(teacher-kiwi)

--- greedy/week2_4_partition_labels/teacher-kiwi.py
+++ greedy/week2_4_partition_labels/teacher-kiwi.py
@@ -26,3 +26,31 @@ class Solution:
 
 # Runtime 40 ms // Beats 70.96%
 # Memory 13.8 MB // Beats 55.91%
+
+####################################################
+# 설탕 쌤 코드 참고
+
+
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        cnt, order = {}, []
+        for i in range(len(s)):
+            if s[i] in cnt:
+                cnt[s[i]][1] = i
+            else:
+                cnt[s[i]] = [i, i]
+                order.append(s[i])
+
+        res = []
+        l, r = 0, 0
+        for c in order:
+            if r + 1 == cnt[c][0]:
+                res.append(r - l + 1)
+                l = r + 1
+            r = max(r, cnt[c][1])
+
+        return res + [r - l + 1]
+
+
+# Runtime 39 ms // Beats 76.11%
+# Memory 13.9 MB // Beats 55.91%
