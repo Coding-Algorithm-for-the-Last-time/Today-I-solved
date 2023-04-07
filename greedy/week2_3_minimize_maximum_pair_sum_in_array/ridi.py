b22397abd293e0e3462491ba5ac61454a75d402b
Ridealist <cjsn92@gmail.com>
2023-04-07 16:20:13 +0900

    greedy_2nd_week: 3번 문제 풀이 추가

---
+++ greedy/week2_3_minimize_maximum_pair_sum_in_array/ridi.py
@@ -0,0 +1,14 @@
+from typing import List
+
+class Solution:
+    def minPairSum(self, nums: List[int]) -> int:
+        nums.sort()
+        n = len(nums)
+        cur_max = 0
+
+        for i in range(n):
+            s = nums[i] + nums[n - 1 - i]
+            if s > cur_max:
+                cur_max = s
+
+        return cur_max        
