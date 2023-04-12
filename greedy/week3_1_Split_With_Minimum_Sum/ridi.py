acfddcffb0cec95c1cdd50c2e9bd286780bd64d4
Ridealist <cjsn92@gmail.com>
2023-04-12 21:53:12 +0900

    TIL: 그리디 3주차_1,2번 문제 리팩터링

--- greedy/week3_1_Split_With_Minimum_Sum/ridi.py
+++ greedy/week3_1_Split_With_Minimum_Sum/ridi.py
@@ -8,18 +8,16 @@ class Solution:
         
         l.sort()
         place = len(l) // 2
-        if len(l) % 2 == 0:
-            for i in range(0, len(l), 2):
-                res += (10 ** (place - 1)) * (l[i] + l[i+1])
-                place -= 1
-        else:
+        if len(l) % 2 != 0:
             pop = l.pop(0)
-            res += (10 ** place) * pop
-            for i in range(0, len(l), 2):
-                res += (10 ** (place - 1)) * (l[i] + l[i+1])
-                place -= 1
+            res += (10 ** place) * pop        
+        for i in range(0, len(l), 2):
+            res += (10 ** (place - 1)) * (l[i] + l[i+1])
+            place -= 1
 
         return res
 
-    ## so so. Runtime Beats 88.79% / Memory Beats 46.97%
+    # Runtime 27ms Beats 88.79%
+    # Memory 13.8MB Beats 94.15%
+
     #TODO 리팩터링!
