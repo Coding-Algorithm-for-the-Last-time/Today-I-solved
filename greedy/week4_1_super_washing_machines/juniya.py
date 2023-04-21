56e551b55d0416b6b3406993a18729949cc211bc
jun <lhj881023@gmail.com>
2023-04-21 21:31:08 +0900

    4주문제1번만해결

--- greedy/week4_1_super_washing_machines/juniya.py
+++ greedy/week4_1_super_washing_machines/juniya.py
@@ -1,16 +1,14 @@
+class Solution:
+    def findMinMoves(self, machines: List[int]) -> int:
+        if sum(machines) % len(machines) != 0:
+            return -1
 
+        avg = sum(machines) // len(machines)
 
-machines = [0,0,11,5]
+        move = 0
+        diff = 0
+        for i in range(len(machines)):
+            diff += machines[i] - avg
+            move = max(move, abs(diff), machines[i] - avg)
 
-def findMinMoves(machines):
-    if sum(machines) % len(machines) != 0:
-        return -1
-    avg = sum(machines) // len(machines)
-    move = 0
-    diff = 0
-    for i in range(len(machines)):
-        diff += machines[i] - avg
-        move = max(move, abs(diff), machines[i] - avg)
-    return move
-
-findMinMoves(machines)
+        return move
