95801c850d0550a83ed070a6bf9fdb6e3b2e9ebe
nlom0218 <nlom0218@naver.com>
2023-04-22 09:00:40 +0900

    docs: 4주차 문제 풀이

---
+++ greedy/week4_1_super_washing_machines/hd.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} machines
+ * @return {number}
+ */
+var findMinMoves = function (machines) {
+  const allDresses = machines.reduce((acc, cur) => acc + cur, 0);
+
+  if (allDresses % machines.length !== 0) return -1;
+
+  const average = allDresses / machines.length;
+  const max = Math.max(...machines) - average;
+
+  return machines.reduce(
+    (acc, cur) => {
+      let [max, sum] = acc;
+      sum += cur - average;
+      max = Math.max(max, Math.abs(sum));
+      return [max, sum];
+    },
+    [max, 0]
+  )[0];
+};
