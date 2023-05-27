494b19b88947944cb121e34590a32673643c11f9
hoetaek <hoetaek@student.snue.ac.kr>
2023-05-27 12:52:48 +0900

    Delete greedy directory

--- greedy/week1_minimum_sum_of_four_digit_number_after_splitting_digits/hoetaek.py
+++
@@ -1,5 +0,0 @@
-class Solution:
-    def minimumSum(self, num: int) -> int:
-        num_str = str(num)
-        num_li = sorted(num_str)
-        return int(num_li[0] + num_li[2]) + int(num_li[1] + num_li[3])
--- greedy/week1_minimum_sum_of_four_digit_number_after_splitting_digits/seoltang.js
+++
@@ -1,8 +0,0 @@
-/**
- * @param {number} num
- * @return {number}
- */
-var minimumSum = function (num) {
-  const nums = [...num.toString()].map((num) => Number(num)).sort((a, b) => a - b);
-  return (nums[0] + nums[1]) * 10 + nums[2] + nums[3];
-};
