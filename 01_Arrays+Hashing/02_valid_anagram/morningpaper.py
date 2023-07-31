4c02016e9abd458c6c0e755828d08eca26ccaee2
hoetaek <rhoetaek@gmail.com>
2023-08-01 08:24:04 +0900

    01 08 HT

--- 01_Arrays+Hashing/02_valid_anagram/morningpaper.py
+++
@@ -1,8 +0,0 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        num_to_index = {}
-        for i, num in enumerate(nums):
-            complement = target - num
-            if complement in num_to_index:
-                return [num_to_index[complement], i]
-            num_to_index[num] = i
