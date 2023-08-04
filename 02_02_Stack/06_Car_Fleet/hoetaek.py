9a86dbcb91d361e9f91b63a534df59c262fd6e54
hoetaek <rhoetaek@gmail.com>
2023-08-04 10:56:17 +0900

    02 02 06 ht

---
+++ 02_02_Stack/06_Car_Fleet/hoetaek.py
@@ -0,0 +1,14 @@
+class Solution:
+    def carFleet(self, target: int, position: list[int], speed: list[int]) -> int:
+        cars = []
+        for i in range(len(position)):
+            count = target -  position[i]) / speed[i]
+            cars.append((position[i], count))
+        cars.sort(key=lambda x: x[0], reverse=True)
+        maxV = 0
+        res = 0
+        for car in cars:
+            if car[1] > maxV:
+                maxV = car[1]
+                res += 1
+        return res
