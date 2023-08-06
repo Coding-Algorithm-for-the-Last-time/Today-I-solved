e36c2547236b4faf37cbe26f39469365d61be71e
kiwi <kwi07@hanmail.net>
2023-08-07 08:31:50 +0900

    Complete Linked List 07(kiwi)

---
+++ 03_03_Linked_List/07_Linked_List_Cycle/kiwi1.py
@@ -0,0 +1,17 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def hasCycle(self, head: ListNode | None) -> bool:
+        node_set = set()
+        while head:
+            if head not in node_set:
+                node_set.add(head)
+                head = head.next
+            else:
+                return True
+        return False
