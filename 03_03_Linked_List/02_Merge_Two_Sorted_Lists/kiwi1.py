ddbc981e45de0d61d280d889b3f9442e51d5f049
kiwi <kwi07@hanmail.net>
2023-08-07 07:55:37 +0900

    Completed Linked List 02(kiwi)

---
+++ 03_03_Linked_List/02_Merge_Two_Sorted_Lists/kiwi1.py
@@ -0,0 +1,18 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def mergeTwoLists(
+        self, list1: ListNode | None, list2: ListNode | None
+    ) -> ListNode | None:
+        if not list1 or (list2 and list1.val > list2.val):
+            list1, list2 = list2, list1
+
+        if list1:
+            list1.next = self.mergeTwoLists(list1.next, list2)
+
+        return list1
