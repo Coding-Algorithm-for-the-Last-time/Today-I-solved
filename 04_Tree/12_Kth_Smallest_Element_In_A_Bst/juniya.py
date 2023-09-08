cfa70688cbebe882dbcc8386991e3d5cb9ab70b1
jun <lhj881023@gmail.com>
2023-09-09 08:32:54 +0900

    junyia풀이

---
+++ 04_Tree/12_Kth_Smallest_Element_In_A_Bst/juniya.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        res = []    
+        
+        def dfs(node):
+            if node is None:
+                return
+            
+            dfs(node.left)
+            res.append(node.val)
+            dfs(node.right)
+
+        dfs(root)
+        res.sort()
+
+        return res[k-1]
