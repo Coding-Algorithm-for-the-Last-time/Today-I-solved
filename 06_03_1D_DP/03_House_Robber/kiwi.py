eb827564b7e436fd33cf961b86bdcbae8ac092ab
kiwi <kwi07@hanmail.net>
2023-10-20 10:33:33 +0900

    Solve 1d dp q3(kiwi)

---
+++ 06_03_1D_DP/03_House_Robber/kiwi.py
@@ -0,0 +1,8 @@
+class Solution:
+    def rob(self, nums: list[int]) -> int:
+        rob1, rob2 = 0, 0
+        for num in nums:
+            temp = max(rob1 + num, rob2)
+            rob1 = rob2
+            rob2 = temp
+        return rob2
